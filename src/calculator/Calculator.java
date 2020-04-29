34254a77d716995d685aa393dc5a15f77a5d107c
Natalie Mulodjanov <ntaliemulodjanov@gmail.com>
2020-04-29 14:48:23 -0400

    created menu of options and start of application

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -5,6 +5,8 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Natalie Mulodjanov 
@@ -12,7 +14,34 @@ package calculator;
 public class Calculator {
 
     public static void main(String[] args) {
-       
+        
+        System.out.println("**------------**"+
+                           "\n|              |"+
+                           "\n|  Calculator  | "+
+                           "\n|              |"+
+                           "\n**------------**");
+        System.out.println("This calculator will solve day to day calculations you might need to do. ");
+        System.out.println("Press 1 to continue show the menu of options...");
+        
+        Scanner input = new Scanner(System.in);
+        while(! input.hasNextInt()){
+            input.nextLine();
+            System.out.println("Please press 1 to show the menu of options");
+        }
+        
+        while(!input.hasNext("1")){
+            input.nextLine();
+            System.out.println("Please press 1 to show the menu of options");
+        }
+        int number = input.nextInt();
+        System.out.println("MENU OF OPTIONS: " +
+                            "\nPress 1 for simple calculations(addition, subtraction, multiplication or division)" +
+                            "\nPress 2 for Perimetre of various shapes " + 
+                            "\nPress 3 for Area of various shapes " +
+                            "\nPress 4 for Quebec Tax calculator" + 
+                            "\nPress 5 for Currency calculator" + 
+                            "\nPress 6 for Calories Burnt Calculator" );
+
     }
-    
-}
+   
+}//end of class 
