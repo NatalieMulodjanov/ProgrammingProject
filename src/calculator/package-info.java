4aa5f1928eb39dc1309760d4e6a70e5098c2a508
Natalie Mulodjanov <ntaliemulodjanov@gmail.com>
2020-05-10 02:04:01 -0400

    final commit

---
+++ src/calculator/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ * This program is my final project of programming 1. It is a calculator that will 
+ * calculate some day to day calculations. 
+ */
